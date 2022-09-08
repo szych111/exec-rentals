cb5b08c89daf87efd3152fb3c7f21a50eab269f6
Alex <67001075+szych111@users.noreply.github.com>
2022-09-08 23:01:11 +0200

    burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-    $('h1').click(function(){
-        console.log('click')
-    })
+const container = document.querySelector(".container");
+const navOpen = container.querySelector(".open-nav");
+const navClose = container.querySelector(".close-nav");
+const navLinks = Array.from(container.querySelectorAll(".nav-link"));
+const nav = container.querySelector(".nav");
 
+navOpen.addEventListener("click", () => {
+  container.classList.add("nav-open");
+});
 
-  });
+navClose.addEventListener("click", () => {
+  container.classList.remove("nav-open");
+});
